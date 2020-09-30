2a10223006e5a92ad11176517b02e0060bc5e522
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-09-30 17:03:28 +0300

    Add bootstrap jquery popper

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :groups
   resources :transactions
   resources :users
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
