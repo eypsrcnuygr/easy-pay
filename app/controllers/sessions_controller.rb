12c5f7e7e448f889ad819eacb1b050840c63e0c7
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-01 00:21:03 +0300

    Add transaction list to users page and link back to transactions after a new transaction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = 'Logged in succesfuly'
-      redirect_to users_path
+      redirect_to user_path(user)
     else
       flash.now[:alert] = 'There was something wrong with your login information'
       render 'new'
